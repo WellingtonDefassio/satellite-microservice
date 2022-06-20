fd493fb5421a3ef8481d3fd91e29f1fc19da28ea
Wellington Defassio <well.defassio@gmail.com>
2022-06-20 09:13:56 -0300

    feat: create satellite module service e controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SatelliteModule } from './satellite/satellite.module';
 
 @Module({
-  imports: [],
+  imports: [SatelliteModule],
   controllers: [AppController],
   providers: [AppService],
 })
